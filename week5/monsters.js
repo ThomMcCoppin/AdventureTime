7bb5eacbe9481c06483ea9749609049ef1475397
ThomMcCoppin <70352712+ThomMcCoppin@users.noreply.github.com>
2020-09-29 13:14:03 -0500

    Add files via upload

---
+++ week5/monsters.js
@@ -0,0 +1,5 @@
+var monsters = [
+	Kobold = {name: "Kobold", hp: 20, damage: 5 },
+	displacerBeast = {name: "Displacer Beast", hp: 50, damage: 10 },
+	Beholder = {name: "Beholder", hp: 80, damage: 20 }
+]
